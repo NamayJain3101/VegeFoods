04124d4cf7e422b7d8ff3747d5f23666a6f81c77
namayjain3101 <namayjain.jainnamay@gmail.com>
2021-01-01 10:12:53 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*', (req, res) => {
+        req.acceptsEncodings('gzip', 'br', 'compress')
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 } else {
